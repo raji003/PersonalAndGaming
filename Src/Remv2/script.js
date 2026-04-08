cafa180fb15f24a830af1e1449a1bf7e5a64e17f
raja <akhtar_hamza@hotmail.com>
2026-04-08 06:51:05 +0200

    changes to be committed:   modified:   Src/Remv2/index.html   modified:   Src/Remv2/script.js   modified:   Src/Remv2/styles.css

--- Src/Remv2/script.js
+++ Src/Remv2/script.js
@@ -1,4 +1,3 @@
-const cards = document.querySelectorAll('.card');
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
@@ -8,7 +7,62 @@ const observer = new IntersectionObserver((entries) => {
     });
 }, { threshold: 0.1 });
 
-cards.forEach((card, index) => {
-    observer.observe(card);
-    card.style.transitionDelay = `${index * 0.05}s`;
+// Particle system for flashiness
+const canvas = document.getElementById('particle-canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const particles = [];
+const particleCount = 100;
+
+class Particle {
+    constructor() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.size = Math.random() * 2 + 1;
+        this.speedX = Math.random() * 2 - 1;
+        this.speedY = Math.random() * 2 - 1;
+        this.color = Math.random() > 0.5 ? '#00f0ff' : '#ff3c6e';
+        this.opacity = Math.random() * 0.5 + 0.2;
+    }
+
+    update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+
+        if (this.x > canvas.width) this.x = 0;
+        if (this.x < 0) this.x = canvas.width;
+        if (this.y > canvas.height) this.y = 0;
+        if (this.y < 0) this.y = canvas.height;
+    }
+
+    draw() {
+        ctx.globalAlpha = this.opacity;
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
+    }
+}
+
+for (let i = 0; i < particleCount; i++) {
+    particles.push(new Particle());
+}
+
+function animateParticles() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    particles.forEach(particle => {
+        particle.update();
+        particle.draw();
+    });
+    requestAnimationFrame(animateParticles);
+}
+
+animateParticles();
+
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 });
